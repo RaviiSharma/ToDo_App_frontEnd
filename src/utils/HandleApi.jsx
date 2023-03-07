b1aae7e329b44761cb73198ee99038c3af04698b
RaviiSharma <engineerravi036@gmail.com>
2023-03-07 16:29:21 +0530

    frontend

---
+++ src/utils/HandleApi.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const baseUrl = "https://todo-app-api-kjmp.onrender.com";
+
+const getAllToDo = (setToDo) => {
+  axios.get(baseUrl).then(({ data }) => {
+    console.log("data ---> ", data);
+    setToDo(data);
+  });
+};
+
+const addToDo = (text, setText, setToDo) => {
+  axios
+    .post(`${baseUrl}/save`, { text })
+    .then((data) => {
+      console.log(data);
+      toast.success("Task added successfully");
+      setText("");
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+  axios
+    .put(`${baseUrl}/update`, { _id: toDoId, text })
+    .then((data) => {
+      toast.success("Task updated successfully");
+      setText("");
+      setIsUpdating(false);
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteToDo = (_id, setToDo) => {
+  axios
+    .post(`${baseUrl}/delete`, { _id })
+    .then((data) => {
+      console.log(data);
+      toast.success("Task deleted successfully");
+
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo };
